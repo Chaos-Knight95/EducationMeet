ca1fff8ba7b21f9c0c750255bfa149f43d09bc18
Manideep Allani <Manideep.a@Technovert.com>
2020-07-17 13:07:25 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EducationMeet.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationMeet.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
